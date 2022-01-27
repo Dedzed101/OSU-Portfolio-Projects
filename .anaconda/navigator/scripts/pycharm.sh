ddf303b7cb1e4544ce2809c47dc883d601f38e0a
Daniel Bracamontes <59780347+Dedzed101@users.noreply.github.com>
2022-01-26 17:59:49 -0800

    c

---
+++ .anaconda/navigator/scripts/pycharm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. /Users/danielbracamontes/opt/anaconda3/bin/activate /Users/danielbracamontes/opt/anaconda3
+open -a "PyCharm CE" >/Users/danielbracamontes/.anaconda/navigator/scripts/pycharm-out-2.txt 2>/Users/danielbracamontes/.anaconda/navigator/scripts/pycharm-err-2.txt
